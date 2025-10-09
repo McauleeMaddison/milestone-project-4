48209390cccaaa6bf11a8a62fba3975965c06fd6
Mcaulee Maddison <vuz9wv37@students.codeinstitute.net>
2025-10-09 14:45:47 +0000

    manage.py added

--- icedinsights/dashboard/models.py
+++ icedinsights/dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CoffeeTrend(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    is_premium = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Subscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {'Active' if self.active else 'Inactive'}"
