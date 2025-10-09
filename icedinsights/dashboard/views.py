055acbde6873114d5e31ac3d81ebc2c0963d4e25
Mcaulee Maddison <vuz9wv37@students.codeinstitute.net>
2025-10-09 13:57:06 +0000

    dashbored folder added

--- icedinsights/dashboard/views.py
+++ icedinsights/dashboard/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def trends(request):
+    ...
+
+def create_checkout_session(request):
+    ...
+
+def stripe_webhook(request):
+    ...
+
+def success(request):
+    ...
+
+def cancel(request):
+    ...
