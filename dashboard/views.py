f031ce22f64133baeec0816a7d60960f5ba007b6
Mcaulee Maddison <vuz9wv37@students.codeinstitute.net>
2025-10-22 14:08:13 +0000

    Fix static dir warning

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from products.models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    return render(request, 'dashboard/home.html', {'products': products})
