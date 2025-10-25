f7d96ee13b8538062baa1ab85b37e9199890c346
Mcaulee Maddison <vuz9wv37@students.codeinstitute.net>
2025-10-25 17:35:51 +0000

    templates/dashboard structue fixed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('customize/<int:product_id>/', views.customize_product, name='customize_product'),
-    path('settings/', views.user_settings, name='user_settings')
 ]
