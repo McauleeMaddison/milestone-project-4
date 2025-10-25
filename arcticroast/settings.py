6fd244e021e108e99c70a1de021012bcc4784e58
Mcaulee Maddison <vuz9wv37@students.codeinstitute.net>
2025-10-25 18:10:01 +0000

    URL fixed

--- arcticroast/settings.py
+++ arcticroast/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # --- SECURITY ---
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-change-this-dev-key")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
