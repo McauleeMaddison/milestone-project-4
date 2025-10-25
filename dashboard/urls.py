e35bb02dbb8334bbfbf86b132bde29772cd377c0
Mcaulee Maddison <vuz9wv37@students.codeinstitute.net>
2025-10-25 16:45:07 +0000

    path fixed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('customize/<int:product_id>/', views.customize_product, name='customize_product'),
-    path('dashboard/', views.dashboard, name='dashboard'),
     path('settings/', views.user_settings, name='user_settings')
 ]
