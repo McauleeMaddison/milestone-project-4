5a691409bcdbfc561e9648e48402fd0cd7df2f14
Mcaulee Maddison <vuz9wv37@students.codeinstitute.net>
2025-10-26 15:28:17 +0000

    Final working version with fixes and UI updates

--- arcticroast/settings.py
+++ arcticroast/settings.py
@@ -82,7 +82,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "dashboard/static/admin/css/style.css"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
