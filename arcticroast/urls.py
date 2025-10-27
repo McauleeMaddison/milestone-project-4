4db6de0decc6c9ca719676a04e1b04f6453fed03
Mac <your-github-email-or-noreply@users.noreply.github.com>
2025-10-27 18:35:00 +0000

    urls.py adjusted

--- arcticroast/urls.py
+++ arcticroast/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('products/', include('apps.products.urls')),
     path('payments/', include('apps.payments.urls')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
